b118db80a7cfef229a5056119e418c479f9021dd
Kádár Ákos <akos@onyx.uvt.nl>
2022-03-11 16:12:52 +0100

    disentangle mention detector and coref scorer

--- run.py
+++ run.py
@@ -14,24 +14,25 @@ from coref.cluster_checker import ClusterChecker
 from coval import Evaluator, get_cluster_info, b_cubed, muc, ceafe, lea
 from thinc.api import require_gpu
 from thinc.api import Adam
-from coref.utils import _load_config
+from coref.utils import _load_config, prfscore
 from coref.thinc_funcs import configure_pytorch_modules, doc2tensors
-from coref.thinc_funcs import spaCyRoBERTa
-from coref.thinc_funcs import _clusterize, predict_span_clusters
+from coref.thinc_funcs import spaCyRoBERTa, _clusterize
+from coref.thinc_funcs import detect_mention_heads, predict_span_clusters
 from coref.thinc_funcs import load_state, save_state
 from coref.thinc_loss import coref_loss, span_loss, mention_loss
-from convert_to_spacy import load_spacy_data
+from process_litbank import load_docs
 
 
 def train(
     config,
     model,
     span_predictor,
+    mention_detector
 ):
     """
     Trains all the trainable blocks in the model using the config provided.
     """
-    docs = load_spacy_data('train.spacy')
+    docs = load_docs('train-litbank.spacy')
     optimizer = Adam(config.learning_rate)
     docs_ids = list(range(len(docs)))
     best_val_score = 0
@@ -52,12 +53,13 @@ def train(
                 doc
             )
             word_features, _ = encoder([doc], False)
+            word_features = word_features[0]
+            mention_scores, m_backprop = mention_detector.begin_update(word_features)
             # Run CorefScorer
             (
                 coref_scores,
-                mention_scores,
                 top_indices
-            ), backprop = model.begin_update(word_features[0])
+            ), c_backprop = model.begin_update(word_features)
             # Compute coref loss
             c_loss, c_grads = coref_loss(
                 model,
@@ -70,16 +72,17 @@ def train(
                 mention_scores,
                 mention_labels
             )
-            # Update CorefScorer
-            backprop((c_grads, m_grads))
+            # Update CorefScorer and MentionDetector
+            c_backprop(c_grads)
+            m_backprop(m_grads)
             model.finish_update(optimizer)
-            # Get data for SpanPredictor
-            # (heads, starts, ends), _ = span_provider(doc, False)
+            mention_detector.finish_update(optimizer)
+            # Maybe run span-predictor
             if starts.size and ends.size:
                 span_scores, backprop_span = span_predictor.begin_update(
                     (
                         sent_ids,
-                        word_features[0],
+                        word_features,
                         heads
                     )
                 )
@@ -112,11 +115,22 @@ def train(
             )
 
         model.attrs['epochs_trained'] += 1
-        val_score = evaluate(config, model, span_predictor)
+        val_score = evaluate(
+            config,
+            model,
+            span_predictor,
+            mention_detector
+        )
         if val_score > best_val_score:
             best_val_score = val_score
             print("New best {}".format(best_val_score))
-            save_state(model, span_predictor, optimizer, config)
+            save_state(
+                model,
+                span_predictor,
+                mention_detector,
+                optimizer,
+                config
+            )
 
 
 @torch.no_grad()
@@ -124,13 +138,14 @@ def evaluate(
     config,
     model,
     span_predictor,
+    mention_detector,
     data_split: str = "dev",
     word_level_conll: bool = False
 ) -> Tuple[float, Tuple[float, float, float]]:
 
     encoder = spaCyRoBERTa()
     encoder.initialize()
-    docs = load_spacy_data('dev.spacy')
+    docs = load_docs('dev-litbank.spacy')
     n_docs = len(docs)
     running_loss = 0.0
     w_checker = ClusterChecker()
@@ -145,6 +160,9 @@ def evaluate(
     bcubed_score = 0.
     ceafe_score = 0.
     lea_score = 0.
+    mention_head_p = 0.
+    mention_head_r = 0.
+    mention_head_f = 0.
 
     pbar = tqdm.tqdm(docs, unit="docs", ncols=0)
     for i, doc in enumerate(pbar):
@@ -154,9 +172,14 @@ def evaluate(
             doc
         )
         word_features, _ = encoder([doc], False)
+        word_features = word_features[0]
         # Get data for SpanPredictor
         # Run CorefScorer
-        coref_scores, mention_scores, top_indices = model.predict(word_features[0])
+        coref_scores, top_indices = model.predict(word_features)
+        mention_pred_heads = detect_mention_heads(
+            mention_detector,
+            word_features
+        )
         # Compute coreference loss
         c_loss, c_grads = coref_loss(
             model,
@@ -174,14 +197,14 @@ def evaluate(
             span_scores = span_predictor.predict(
                 (
                     sent_ids,
-                    word_features[0],
+                    word_features,
                     heads
                 )
             )
             span_clusters = predict_span_clusters(
                 span_predictor,
                 sent_ids,
-                word_features[0],
+                word_features,
                 word_clusters
             )
             pred_starts = span_scores[:, :, 0].argmax(axis=1)
@@ -206,6 +229,15 @@ def evaluate(
         bcubed_score += bcubed_evaluator.get_f1()
         ceafe_score += ceafe_evaluator.get_f1()
         lea_score += lea_evaluator.get_f1()
+        predicted_mentions = mention_detector.ops.alloc1f(len(word_features))
+        predicted_mentions[mention_pred_heads] = 1.0
+        m_p, m_r, m_f = prfscore(
+            predicted_mentions,
+            mention_labels
+        )
+        mention_head_p += m_p
+        mention_head_r += m_r
+        mention_head_f += m_f
 
         pbar.set_description(
             f"{data_split}:"
@@ -225,6 +257,9 @@ def evaluate(
     print("MUC", muc_score/n_docs)
     print("CEAFE", ceafe_score/n_docs)
     print("BCUB", bcubed_score/n_docs)
+    print("Mention head precision:", m_p/n_docs)
+    print("Mention head recall:", m_r/n_docs)
+    print("Mention head f-score:", m_f/n_docs)
     eval_score = w_lea[0] + s_lea[0]
     return eval_score
 
@@ -274,34 +309,49 @@ if __name__ == "__main__":
                                 " If not supplied, in 'eval' mode the latest"
                                 " weights of the experiment will be loaded;"
                                 " in 'train' mode no weights will be loaded.")
+    argparser.add_argument("--mention-weights",
+                           help="Path to file with weights to load."
+                                " If not supplied, in 'eval' mode the latest"
+                                " weights of the experiment will be loaded;"
+                                " in 'train' mode no weights will be loaded.")
     argparser.add_argument("--word-level", action="store_true",
                            help="If set, output word-level conll-formatted"
                                 " files in evaluation modes. Ignored in"
                                 " 'train' mode.")
     args = argparser.parse_args()
+    config = _load_config(args.config_file, args.experiment)
 
     if args.batch_size:
         config.a_scoring_batch_size = args.batch_size
 
     require_gpu()
     seed(2020)
-    config = _load_config(args.config_file, args.experiment)
-    model, span_predictor = configure_pytorch_modules(config)
-    
+    model, span_predictor, mention_detector = configure_pytorch_modules(config)
+
     if args.mode == "train":
         model.attrs['epochs_trained'] = 0
         with output_running_time():
-            train(config, model, span_predictor)
+            train(
+                config,
+                model,
+                span_predictor,
+                mention_detector
+            )
     else:
         model, span_predictor = load_state(
             model,
             span_predictor,
+            mention_detector,
             config,
             args.coref_weights,
             args.span_weights,
+            args.mention_weights
         )
-        evaluate(config,
-                 model,
-                 span_predictor,
-                 data_split=args.data_split,
-                 word_level_conll=args.word_level)
+        evaluate(
+            config,
+            model,
+            span_predictor,
+            mention_detector,
+            data_split=args.data_split,
+            word_level_conll=args.word_level
+        )
